ca0100f6cca4719c406cfe935863c0cf50bbe869
RalphTro <ralph.troeger@gs1.de>
2021-07-25 12:42:07 +0200

    First working prototype

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,14 @@
 "use strict"
 
 /**
- * Returns check digit for GTIN-8, GTIN-12, GTIN-13, GLN, GTIN-14, SSCC, GSIN, GSRN, GDTI, GCN
+ * Returns check digit for GTIN-8, GTIN-12, GTIN-13, GLN, GTIN-14, SSCC, GSIN, GSRN, GSRN-P
  * Returns null if input (GS1 key without check digit) is invalid
  * For further details, see GS1 GenSpecs, section 7.9.1: Standard check digit calculations for GS1 data structures
  * @param {string} keyWoCD - GS1 key without check digit
  * @returns {string | none} - Check digit for GS1 key or 'none' if keyWoCD is invalid
  */
 
-let checkDigit = (keyWoCD) => {
+ let checkDigit = (keyWoCD) => {
     /* Check that input string conveys number of digits that correspond to a given GS1 key */
     if (/(^\d{7}$)|(^\d{11}$)|(^\d{12}$)|(^\d{13}$)|(^\d{16}$)|(^\d{17}$)/.test(keyWoCD) === false){
         return null
@@ -36,18 +36,106 @@ let checkDigit = (keyWoCD) => {
     return checkDigit
     }
 }
-    
-console.log(checkDigit('401234512345')) /* Expected result: 6 */
-console.log(checkDigit('0401234512345')) /* Expected result: 6 */
-console.log(checkDigit('34012345111111111')) /* Expected result: 1 */
-console.log(checkDigit('0123456')) /* Expected result: 5 */
-console.log(checkDigit('0000000000000')) /* Expected result: 0 */
-console.log(checkDigit('9999999999999')) /* Expected result: 7 */
-console.log(checkDigit('06141415555')) /* Expected result: 7 */
-console.log(checkDigit('4023333987654000'))  /* Expected result: 9 */
-console.log(checkDigit('12345567876543123'))  /* Expected result:  4 */
-console.log(checkDigit('401234566666'))  /* Expected result:  3 */
-console.log(checkDigit('1234'))  /* Expected result: null */
-console.log(checkDigit('12345678910a'))  /* Expected result: null */
-console.log(checkDigit('ab'))  /* Expected result: null */
-console.log(checkDigit('11111111111111111111'))  /* Expected result: null */
+
+document.addEventListener("DOMContentLoaded", () => {
+    const calcButton = document.getElementById("calculate")
+    let inputString = document.getElementById("input")
+    let outputValue = document.getElementById("output")
+    let outputKey = document.getElementById("keyWithCD")
+    let feedback = document.getElementById("feedback")
+    /* Make sure that user only inserts digits in input field */
+    inputString.addEventListener("keydown", (e) => {
+        console.log('e.key: ', e.key)
+        if (e.key.match(/[\d]/)){ 
+            feedback.value = ""
+        }else{
+            feedback.value = "Only digits permitted. Remove invalid characters."
+        }
+    })
+    /* Display check digit/complete GS1 key when clicking calculate-button */
+    calcButton.addEventListener("click", (event) => {
+        event.preventDefault()      
+        if (checkDigit(inputString.value) === null) {
+            outputKey.value = ""
+            outputValue.value = ""
+            alert("Please insert a valid input string length")
+        }else{
+            outputValue.value = checkDigit(inputString.value)
+            outputKey.value = inputString.value + checkDigit(inputString.value)
+        }
+      })
+    /* Enable user to get demo values for each GS1 key */
+    const gtin8Button = document.getElementById("gtin8")
+    const gtin8Demo = "0123456"
+    gtin8Button.addEventListener("click", () => {
+        inputString.value = gtin8Demo
+        outputKey.value = ""
+        outputValue.value = ""
+    }
+    )
+    const gtin12Button = document.getElementById("gtin12")
+    const gtin12Demo = "06141415555"
+    gtin12Button.addEventListener("click", () => {
+        inputString.value = gtin12Demo
+        outputKey.value = ""
+        outputValue.value = ""
+    }
+    )
+    const gtin13Button = document.getElementById("gtin13")
+    const gtin13Demo = "401234512345"
+    gtin13Button.addEventListener("click", () => {
+        inputString.value = gtin13Demo
+        outputKey.value = ""
+        outputValue.value = ""
+    }
+    )
+    const gtin14Button = document.getElementById("gtin14")
+    const gtin14Demo = "0401234512345"
+    gtin14Button.addEventListener("click", () => {
+        inputString.value = gtin14Demo
+        outputKey.value = ""
+        outputValue.value = ""
+    }
+    )
+    const glnButton = document.getElementById("gln")
+    const glnDemo = "401234500000"
+    glnButton.addEventListener("click", () => {
+        inputString.value = glnDemo
+        outputKey.value = ""
+        outputValue.value = ""
+    }
+    )
+    const ssccButton = document.getElementById("sscc")
+    const ssccDemo = "34012345111111111"
+    ssccButton.addEventListener("click", () => {
+        inputString.value = ssccDemo
+        outputKey.value = ""
+        outputValue.value = ""
+    }
+    )
+    const gsinButton = document.getElementById("gsin")
+    const gsinDemo = "4023333987654000"
+    gsinButton.addEventListener("click", () => {
+        inputString.value = gsinDemo
+        outputKey.value = ""
+        outputValue.value = ""
+    }
+    )
+    const gsrnButton = document.getElementById("gsrn")
+    const gsrnDemo = "40123450000000987"
+    gsrnButton.addEventListener("click", () => {
+        inputString.value = gsrnDemo
+        outputKey.value = ""
+        outputValue.value = ""
+    }
+    )
+    const gsrnpButton = document.getElementById("gsrnp")
+    const gsrnpDemo = "40233330000000123"
+    gsrnpButton.addEventListener("click", () => {
+        inputString.value = gsrnpDemo
+        outputKey.value = ""
+        outputValue.value = ""
+    }
+    )
+})
+
